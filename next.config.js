9c30d511e698714c6cd8843275a07275d679187d
Tony Ward <tony.ward@lifeomic.com>
2020-09-18 13:09:57 -0400

    Revert "Merge pull request #8 from lifeomic/gh-pages-deploy-attempt"
    This reverts commit 68e6fcc2bccbcac1190303b5576c9f22895b7c58, reversing
    changes made to cb93f865c936b6f61f19e7b3a4025bcd7f5259d5.
    
    # Conflicts:
    #	.github/workflows/release.yml

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const debug = process.env.NODE_ENV !== 'production';
-
 const withTranspileModules = require('next-transpile-modules')([
   '@lifeomic/chromicons',
 ]);
 
 module.exports = withTranspileModules({
-  assetPrefix: !debug ? '/chromicons.com/' : '',
-
   experimental: {
     // https://github.com/vercel/next.js/issues/8103#issuecomment-586142928
     // https://github.com/vercel/next.js/pull/7704
