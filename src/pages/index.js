dc92fa38c28521a014e6250186feef763415757b
Tony Ward <tony.ward@lifeomic.com>
2020-10-07 16:58:23 -0400

    FEAT: Add Download All link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import { CategoryFilters } from '../components/categoryFilters';
-import { CheckCircle, Flag, Lifeology } from '@lifeomic/chromicons/react/lined';
 import { Chromicons } from '../components/icons/chromicons';
 import { IconModal } from '../components/iconModal';
 import { LifeOmic } from '../components/icons/lifeomic';
@@ -7,9 +6,30 @@ import { SearchField } from '../components/searchField';
 import { Tile } from '../components/tile';
 import { Transition } from '@tailwindui/react';
 import { useState } from 'react';
+import {
+  CheckCircle,
+  Download,
+  Flag,
+  Lifeology,
+} from '@lifeomic/chromicons/react/lined';
 import * as allLinedChromicons from '@lifeomic/chromicons/react/lined';
+import clsx from 'clsx';
 import Head from 'next/head';
 import metadata from '../util/metadata';
+
+const HeroNavLink = ({ className, children, ...rootProps }) => {
+  return (
+    <a
+      className={clsx(
+        'flex items-center px-2 duration-300 ease-in-out transition-opacity hover:opacity-75 focus:outline-none focus-visible:shadow-dark-mode-outline focus-visible:underline',
+        className
+      )}
+      {...rootProps}
+    >
+      {children}
+    </a>
+  );
+};
 
 const getChromicons = () => {
   const iconNames = Object.keys(allLinedChromicons);
@@ -115,9 +135,12 @@ export default function IndexPage({ pkgVersion }) {
               <dd>{pkgVersion}</dd>
             </dl>
           </div>
-          <div className="flex items-center text-white text-sm">
-            <a
-              className="flex items-center duration-300 ease-in-out transition-opacity hover:opacity-75 focus:outline-none focus-visible:shadow-outline focus-visible:underline"
+          <nav
+            className="flex items-center text-white text-sm space-x-2 md:space-x-4"
+            role="navigation"
+            aria-label="Primary Navigation"
+          >
+            <HeroNavLink
               href="https://github.com/lifeomic/chromicons"
               target="_blank"
               rel="noopener noreferrer"
@@ -132,8 +155,15 @@ export default function IndexPage({ pkgVersion }) {
                 <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
               </svg>
               <span className="sr-only sm:not-sr-only">GitHub</span>
-            </a>
-          </div>
+            </HeroNavLink>
+            <HeroNavLink
+              className="sr-only sm:not-sr-only sm:text-white sm:bg-gray-700 sm:rounded-md sm:px-4 sm:py-2 "
+              href="https://github.com/lifeomic/chromicons/archive/master.zip"
+            >
+              <Download className="mr-2 h-4 w-4" role="img" aria-hidden />
+              <span>Download All</span>
+            </HeroNavLink>
+          </nav>
         </div>
         <div className="w-full flex-none text-center space-y-6 font-hero xl:w-auto xl:flex-auto">
           <h1 className="text-white text-2xl leading-9 font-semibold sm:text-4xl sm:leading-10">
