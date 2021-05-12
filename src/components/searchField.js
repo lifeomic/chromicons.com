6f57f4da7f9bef66f73f84c5b5b851dc9e195bcd
Casey Dexter <casey.dexter@lifeomic.com>
2021-05-12 16:36:39 -0400

    Refactor search field and search icon for center alignment

--- src/components/searchField.js
+++ src/components/searchField.js
@@ -4,25 +4,27 @@ import clsx from 'clsx';
 export const SearchField = ({ className, inputClassName, onChange, value }) => {
   return (
     <form
-      className={clsx('relative flex items-center', className)}
+      className={clsx('relative flex items-center justify-center', className)}
       onSubmit={(e) => e.preventDefault()}
     >
       <label className="sr-only" htmlFor="search-icons" aria-hidden>
         Search icons
       </label>
-      <input
-        id="search-icons"
-        type="search"
-        placeholder="Search icons"
-        className={clsx(
-          'appearance-none border border-grey bg-gray-200 rounded-lg pl-10 pr-4 py-2 text-black focus:outline-none focus:border-purple-400',
-          inputClassName
-        )}
-        onChange={onChange}
-        value={value}
-      />
-      <div className="absolute pl-3 flex items-center justify-center">
-        <Search className="fill-current text-grey h-4 w-4" />
+      <div className="relative flex w-full md:w-auto">
+        <input
+          id="search-icons"
+          type="search"
+          placeholder="Search icons"
+          className={clsx(
+            'appearance-none border border-grey bg-gray-200 rounded-lg pl-10 pr-4 py-2 text-black bg-opacity-90 focus:outline-none focus:border-purple-400',
+            inputClassName
+          )}
+          onChange={onChange}
+          value={value}
+        />
+        <div className="absolute flex items-center justify-center mt-3 ml-3">
+          <Search className="fill-current text-grey h-5 w-5" />
+        </div>
       </div>
     </form>
   );
